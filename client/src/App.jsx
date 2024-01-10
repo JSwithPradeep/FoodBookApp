725a20e24b5cf3dd3a200068b579c8edc475bb87
JSwithPradeep <130471506+JSwithPradeep@users.noreply.github.com>
2024-01-09 22:46:46 -0800

    foodApp

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+import Categories from './component/Categories'
+import Delivery from './component/Delivery'
+import Feature from './component/Feature'
+import Footer from './component/Footer'
+
+import Meal from './component/Meal'
+import NewsLatter from './component/NewsLatter'
+import TopNav from './component/TopNav'
+import TopPics from './component/TopPics'
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <TopNav/>
+        <Feature/>
+
+        <Delivery/>
+        <TopPics/>
+        <Meal/>
+        <Categories/>
+        <NewsLatter/>
+        <Footer/>
+        
+      
+      
+    </>
+  )
+}
+
+export default App
